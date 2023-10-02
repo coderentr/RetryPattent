fe4da804f5d5066f0843bf8478f234bfafc7a33f
mustafaeren <mstafa.erenn@gmail.com>
2023-10-02 23:30:59 +0300

    retryPolicy example

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿### Retry Pattern
+
+Servisler arası iletişim kurduğumuz noktalarda istekte bulunulan servisin herhengi bir nedenden dolayı cevap vermediği yada hata döndüğü
+durumlarda isteği tekrarlamak, timeout işlemleri yada bağlantısını kesmek, sınırlandırmak isteyebiliriz. İşte bu gereksinim doğrultusunda
+.net de retry patten'i polly kütüphanesinden faydalanarak kullanabiliriz.
+
+Geçici ağ sorunları yada hizmet kesintileri olması durumunda sistemimize aşırı yüklenmeden sorunu çözmüş olabiliriz. 
+
+
---
+++ RetryPattent/Controllers/ExampleController.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+using Polly;
+
+
+namespace RetryPattent.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ExampleController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<ExampleController> _logger;
+   
+    public ExampleController(ILogger<ExampleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetData")]
+    public async Task<IActionResult> Get()
+    {
+        var retryPolicy = Policy
+            .Handle<Exception>()
+            .RetryAsync(4, onRetry: (exception, retryCount) =>
+            {
+                Console.WriteLine($"Error : {exception.Message} ... retryCount :{retryCount} ");
+            });
+
+        await retryPolicy.ExecuteAsync(async () =>
+        {
+            await ConnectToApi();
+        });
+
+
+        return Ok();
+    }
+
+    private async Task ConnectToApi()
+    {
+        try
+        {
+            var apiUrl = "https://localhost/api/example";
+            var client = new HttpClient();
+            var response = client.GetAsync(apiUrl).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine($"Error {response.StatusCode}");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Error {ex.Message}");
+        }
+    }
+}
+
